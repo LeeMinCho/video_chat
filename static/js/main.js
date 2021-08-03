615993f3337de4ff63e8178c3fa39cfac1c1e9e4
ekayunan <ekayunan98@gmail.com>
2021-08-03 14:35:33 +0700

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,59 @@
+console.log('main js');
+
+var labelUsername = document.querySelector('#label-username');
+var usernameInput = document.querySelector('#username');
+var btnJoin = document.querySelector('#btn-join');
+
+var username;
+
+var webSocket;
+
+function webSocketOnMessage(event) {
+    var parsedData = JSON.parse(event.data);
+    var message = parsedData["message"];
+
+    console.log("message: " + message);
+}
+
+btnJoin.addEventListener('click', () => {
+    username = usernameInput.value;
+
+    console.log(username);
+
+    if (username == "") {
+        return;
+    }
+
+    usernameInput.value = "";
+    usernameInput.disabled = true;
+    usernameInput.style.visibility = "hidden";
+    
+    btnJoin.disabled = true;
+    btnJoin.style.visibility = "hidden";
+
+    var labelUsername = document.querySelector('#label-username');
+    labelUsername.innerHTML = username;
+
+    var loc = window.location;
+    var wsStart = "ws://";
+
+    if (loc.protocol == "https:") {
+        wsStart = "wss://";
+    }
+
+    var endPoint = wsStart + loc.host + loc.pathname;
+
+    console.log("endPoint: " + endPoint);
+
+    webSocket = new WebSocket(endPoint);
+
+    webSocket.addEventListener("open", (e) => {
+        console.log("Connection Open!");
+    });
+    webSocket.addEventListener("close", (e) => {
+        console.log("Connection Closed!");
+    });
+    webSocket.addEventListener("error", (e) => {
+        console.log("Error!");
+    });
+})
